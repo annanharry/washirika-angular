65848dd76055fe1a5a1cc3c8f27e3fa7dbcd7a50
annanharry <annanharry7@gmail.com>
2023-02-19 16:35:44 +0300

    Initialise login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
