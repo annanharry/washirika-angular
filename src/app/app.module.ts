6c18cc3ad6741ab3c4998d22d61b576edef4969f
annanharry <annanharry7@gmail.com>
2023-02-27 14:33:56 +0300

    authentication init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,16 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -29,10 +32,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'washirika-0',
+    appId: '1:1077636634232:web:dd81ddacc41f1e3e6fff8d',
+    storageBucket: 'washirika-0.appspot.com',
+    apiKey: 'AIzaSyAz41VHXaMcDf1IU6BhbMNXmMa_ZvjB4Xg',
+    authDomain: 'washirika-0.firebaseapp.com',
+    messagingSenderId: '1077636634232',
+    measurementId: 'G-LJ20VVQ8FX',
+  },};
